cd1c8e59d8a25abe08a416a2e6c00006b60a1209
Magdalena Gresk <magdagresk@onet.pl>
2018-04-18 22:24:40 -0400

    Modify images and js folders, adjust html and js files

--- js/script.js
+++ js/script.js
@@ -3,25 +3,25 @@ var movies = [
     	id: 1,
     	title: 'James Bond',
     	desc: 'about British spy',
-    	image: src=('image1.jpg')
+    	image: src=('images/image1.jpg')
  	},
  	{
     	id: 2,
     	title: 'Pretty woman',
     	desc: 'romantic comedy',
-    	image: src=('image2.jpg')
+    	image: src=('images/image2.jpg')
 	},
  	{
   		id: 3,
   		title: 'Pippi Longstocking',
   		desc: 'the beloved story of a spunky young girl',
-  		image: src=('image3.png')
+  		image: src=('images/image3.png')
 	},
 	{
 		id: 4,
 		title: 'Jaws',
 		desc: 'thriller about a giant man-eating great white shark',
-		image: src=('image4.jpg')
+		image: src=('images/image4.jpg')
 	}
 ]
 
@@ -34,7 +34,7 @@ var moviesElement = movies.map(function(movie) {
 })
 var element = 
 	React.createElement('div', {},
-		React.createElement('h1', {}, 'lista filmow'),
+		React.createElement('h1', {}, 'my favorite movies'),
 		React.createElement('ul', {}, moviesElement)
 	)
 ReactDOM.render(element, document.getElementById('app'))
